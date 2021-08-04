cb8fcad77168e6a88e1d9fde687650b8b82fa0ea
Sami Merhi <51339977+merhi-odg@users.noreply.github.com>
2021-08-04 10:59:08 -0500

    Update dummy_monitoring_test_results.py

--- dummy_monitoring_test_results.py
+++ dummy_monitoring_test_results.py
@@ -1,6 +1,14 @@
 import json
 
 
+# modelop.init
+def begin():
+    pass
+
+# modelop.score
+def action(data):
+    yield data
+
 # modelop.metrics
 def metrics(df):
     with open("model_test_results.json", "r") as f:
